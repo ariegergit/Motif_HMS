8bc1278f389b20739e08c3d601da1ed66a247835
Stefania Druga <serene@HummingBird.local>
2017-05-16 10:36:06 -0400

    working code

--- sketch_01/sketch_01.ino
+++ sketch_01/sketch_01.ino
@@ -3,7 +3,6 @@
 // include SPI, MP3 and SD libraries
 #include <SPI.h>
 #include <MFRC522.h>//include the RFID reader library
-
 #include <Adafruit_VS1053.h>
 #include <SD.h>
 
@@ -13,20 +12,18 @@
 #define SHIELD_CS     7      // VS1053 chip select pin (output)
 #define SHIELD_DCS    6      // VS1053 Data/command select pin (output)
 
-// These are common pins between breakout and shield
 #define CARDCS 4     // Card chip select pin
-// DREQ should be an Int pin, see http://arduino.cc/en/Reference/attachInterrupt
 #define DREQ 3       // VS1053 Data request, ideally an Interrupt pin
-
-Adafruit_VS1053_FilePlayer musicPlayer = 
-  // create breakout-example object!
-  //Adafruit_VS1053_FilePlayer(BREAKOUT_RESET, BREAKOUT_CS, BREAKOUT_DCS, DREQ, CARDCS);
-  // create shield-example object!
-  Adafruit_VS1053_FilePlayer(SHIELD_RESET, SHIELD_CS, SHIELD_DCS, DREQ, CARDCS);
-
 
 #define SS_PIN 10  //slave select pin
 #define RST_PIN 8  //reset pin
+
+bool playOnce = false;
+
+//initialise player and RFID reader
+
+Adafruit_VS1053_FilePlayer musicPlayer = 
+  Adafruit_VS1053_FilePlayer(SHIELD_RESET, SHIELD_CS, SHIELD_DCS, DREQ, CARDCS);
 MFRC522 mfrc522(SS_PIN, RST_PIN);        // instatiate a MFRC522 reader object.
 MFRC522::MIFARE_Key key;//create a MIFARE_Key struct named 'key'
 
@@ -34,100 +31,66 @@ MFRC522::MIFARE_Key key;//create a MIFARE_Key struct named 'key'
 
 void setup() {
   Serial.begin(9600);
-  mfrc522.PCD_Init();        // Init MFRC522 card (in case you wonder what PCD means: proximity coupling device)
-
-  
+  mfrc522.PCD_Init();   
   if (! musicPlayer.begin()) { // initialise the music player
      Serial.println(F("Couldn't find VS1053, do you have the right pins defined?"));
      while (1);
   }
-  Serial.println(F("Player found"));
   
    if (!SD.begin(CARDCS)) {
     Serial.println(F("SD failed, or not present"));
     while (1);  // don't do anything more
   }
-
-  // list files
-//  printDirectory(SD.open("/"), 0);
-  
   // Set volume for left, right channels. lower numbers == louder volume!
-  musicPlayer.setVolume(20,20);
-
-  // Timer interrupts are not suggested, better to use DREQ interrupt!
-  //musicPlayer.useInterrupt(VS1053_FILEPLAYER_TIMER0_INT); // timer int
-
-  // If DREQ is on an interrupt pin (on uno, #2 or #3) we can do background
-  // audio playing
-  musicPlayer.useInterrupt(VS1053_FILEPLAYER_PIN_INT);  // DREQ int
-  
-  // Play one file, don't return until complete
-  Serial.println(F("Playing track 001"));
-  musicPlayer.playFullFile("track001.mp3");
-  // Play another file in the background, REQUIRES interrupts!
-//  Serial.println(F("Playing track 002"));
-//  musicPlayer.startPlayingFile("track002.mp3");
-        SPI.begin();               // Init SPI bus
-        mfrc522.PCD_Init();        // Init MFRC522 card (in case you wonder what PCD means: proximity coupling device)
-        Serial.println("Scan a Motif card");
-         for (byte i = 0; i < 6; i++) {
-                key.keyByte[i] = 0xFF;//keyByte is defined in the "MIFARE_Key" 'struct' definition in the .h file of the library
-        }
+  musicPlayer.setVolume(10,10);
+  SPI.begin();               // Init SPI bus
+  mfrc522.PCD_Init();        // Init MFRC522 card (in case you wonder what PCD means: proximity coupling device)
+  Serial.println("Scan a Motif card");
+  for (byte i = 0; i < 6; i++) {
+    key.keyByte[i] = 0xFF;//keyByte is defined in the "MIFARE_Key" 'struct' definition in the .h file of the library
+  }
 }
 
 int block=2;//this is the block number we will write into and then read. Do not write into 'sector trailer' block, since this can make the block unusable.
-
 //byte blockcontent[16] = {"motif3"};//an array with 16 bytes to be written into one of the 64 card blocks is defined
 //byte blockcontent[16] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};//all zeros. This can be used to delete a block.
 byte readbackblock[18];//This array is used for reading out a block. The MIFARE_Read method requires a buffer that is at least 18 bytes to hold the 16 bytes of a block.
 
 
 void loop() {
+  mfrc522.PCD_Init(); // Init MFRC522
+  delay(250);
 
    if ( ! mfrc522.PICC_IsNewCardPresent()) {//if PICC_IsNewCardPresent returns 1, a new card has been found and we continue
+    Serial.println("card not found");  
     return;//if it did not find a new card is returns a '0' and we return to the start of the loop
   }
+
+  
 
   // Select one of the cards
   if ( ! mfrc522.PICC_ReadCardSerial()) {//if PICC_ReadCardSerial returns 1, the "uid" struct (see MFRC522.h lines 238-45)) contains the ID of the read card.
     return;//if it returns a '0' something went wrong and we return to the start of the loop
   }
-         Serial.println("card selected");
-
          readBlock(block, readbackblock);//read the block back
-         Serial.print("read block: ");
-         for (int j=0 ; j<16 ; j++)//print the block contents
-         {
-           Serial.write (readbackblock[j]);//Serial.read() transmits the ASCII numbers as human readable characters to serial monitor
-         }
-         Serial.println("");
-  // File is playing in the background
-  if (musicPlayer.stopped()) {
-    Serial.println("Done playing music");
-    while (1);
-  }
-  if (Serial.available()) {
-    char c = Serial.read();
-    
-    // if we get an 's' on the serial console, stop!
-    if (c == 's') {
-      musicPlayer.stopPlaying();
-    }
-    
-    // if we get an 'p' on the serial console, pause/unpause!
-    if (c == 'p') {
-      if (! musicPlayer.paused()) {
-        Serial.println("Paused");
-        musicPlayer.pausePlaying(true);
-      } else { 
-        Serial.println("Resumed");
-        musicPlayer.pausePlaying(false);
-      }
-    }
-  }
+         Serial.println("read block: ");
+         
+          if(playOnce){
+               if (readbackblock[5] == 49) {
+                musicPlayer.playFullFile("track000.mp3");
+                musicPlayer.stopPlaying();
+              }
+              if (readbackblock[5] == 50) {
+              musicPlayer.playFullFile("track001.mp3");    
+              }
+              if (readbackblock[5] == 51) {
+              musicPlayer.playFullFile("track002.mp3"); 
+            }
+            playOnce = false;
+          }
+         
 
-  delay(100);
- 
+  
 }
 
        
@@ -188,6 +151,12 @@ int readBlock(int blockNumber, byte arrayAddress[])
          //Serial.println(mfrc522.GetStatusCodeName(status));
          return 3;//return "3" as error message
   }
+
+
+  if(status == MFRC522::STATUS_OK){
+    playOnce = true;
+  }
+  
   //it appears the authentication needs to be made before every block read/write within a specific sector.
   //If a different sector is being authenticated access to the previous one is lost.
 
@@ -200,47 +169,7 @@ int readBlock(int blockNumber, byte arrayAddress[])
           Serial.print("MIFARE_read() failed: ");
           //Serial.println(mfrc522.GetStatusCodeName(status));
           return 4;//return "4" as error message
-  }
-  Serial.println("block was read");
-   for (int j=0 ; j<16 ; j++){
-    if (readbackblock[j] = "motif1") {
-    musicPlayer.playFullFile("track000.mp3");    
-    }
-    if (readbackblock[j] = "motif2") {
-    musicPlayer.playFullFile("track001.mp3");    
-    }
-     if (readbackblock[j] = "motif3") {
-    musicPlayer.playFullFile("track002.mp3"); 
-
-    }
-   }
-    
+  }    
 }
 
-
-/// File listing helper
-//void printDirectory(File dir, int numTabs) {
-//   while(true) {
-//     
-//     File entry =  dir.openNextFile();
-//     if (! entry) {
-//       // no more files
-//       //Serial.println("**nomorefiles**");
-//       break;
-//     }
-//     for (uint8_t i=0; i<numTabs; i++) {
-//       Serial.print('\t');
-//     }
-//     Serial.print(entry.name());
-//     if (entry.isDirectory()) {
-//       Serial.println("/");
-//       printDirectory(entry, numTabs+1);
-//     } else {
-//       // files have sizes, directories do not
-//       Serial.print("\t\t");
-//       Serial.println(entry.size(), DEC);
-//     }
-//     entry.close();
-//   }
-//}
 
